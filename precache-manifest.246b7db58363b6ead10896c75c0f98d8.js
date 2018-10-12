5b5d669979d6ad5fac0806abf9a1b12499685dc5
Rion <rionirvin>
2018-10-11 19:21:41 -0500

    Updates

---
+++ precache-manifest.246b7db58363b6ead10896c75c0f98d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f99a7f748a54c0d52a3c",
+    "url": "/clicky-app/static/js/runtime~main.f99a7f74.js"
+  },
+  {
+    "revision": "846e963f7b4161ac33f6",
+    "url": "/clicky-app/static/js/main.846e963f.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/clicky-app/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "846e963f7b4161ac33f6",
+    "url": "/clicky-app/static/css/main.1fa7083c.chunk.css"
+  },
+  {
+    "revision": "9ec2bd98ba1024a1c8faa0fdd45960c9",
+    "url": "/clicky-app/index.html"
+  }
+];
